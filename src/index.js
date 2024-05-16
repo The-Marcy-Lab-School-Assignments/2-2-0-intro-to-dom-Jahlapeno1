d5acfb3d0110b668533f9d33aa92e319bec55925
Itzel01 <itzel.areymi.rendon@gmail.com>
2024-05-15 22:18:10 -0400

    feedback

--- src/index.js
+++ src/index.js
@@ -8,3 +8,9 @@ para.id = 'main-text';
 para.className = 'boring-text';
 para.textContent = "Look, I'm some text!";
 body.appendChild(para);
+
+/** FEEDBACK: Great job getting all test cases to pass! 
+ * Here are some questions to sit with, could we have made this code DRYer? 
+ * Could we have used a function? 
+ * Also, we always want to have our runner function so make sure to include that in the future!
+ */
